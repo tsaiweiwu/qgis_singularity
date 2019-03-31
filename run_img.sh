ad656e4e2306188d52a32dfff332c78866d78abf
Tsai-wei Wu <tsaiwei@purdue.edu>
2019-03-30 22:24:32 -0400

    run_img.sh

--- run_img.sh
+++ run_img.sh
@@ -3,13 +3,10 @@
 # sudo singularity shell qgis-server-LTR.simg
 #singularity shell -B /var/opt qgis-server-LTR.simg
 
-#sudo singularity shell -B /var/opt --writable qgis-server-20190209.sandbox
+#sudo singularity shell -B /var/opt --writable qgis-ubuntu.sandbox
 #sudo singularity shell --writable qgis-ubuntu.sandbox
 sudo singularity shell -B /var/opt --writable qgis-ubuntu.sandbox
-#sudo singularity shell --writable mcell-20181129.img
-#sudo singularity shell --writable OpenFOAM2.sandbox
-#sudo singularity shell --writable OpenFOAM2-20181012.img
 
 ## enabling gui
-#singularity shell -B /var/opt OpenFOAM2-20181012.img
+#singularity shell -B /var/opt qgis-20190330.img
 
